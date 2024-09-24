3b502a775f793e9466234aed8cdb2db98310b737
Poorvika <152640996+poorvikaa08@users.noreply.github.com>
2024-09-24 21:59:34 +0530

    update

--- 2798.c
+++
@@ -1,9 +0,0 @@
-int numberOfEmployeesWhoMetTarget(int* hours, int hoursSize, int target){
-    int i,num;
-    num=0;
-    for(i=0;i<hoursSize;i++){
-        if(hours[i]>=target)
-            num++;
-    }
-    return num;
-}
